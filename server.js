9c1b2a2e44f8c72dd0718b7676727e5ce204efc7
chizi205 <vin20464@gamil.com>
2026-08-20 11:22:49 +0700

    update

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ const PORT = process.env.PORT || 3100;
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("Auto-deploy qua GitHub Actions đã hoạt động mượt mà!");
 });
